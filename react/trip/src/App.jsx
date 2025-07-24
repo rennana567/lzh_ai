3168e1f58d0de66ec4d88a04b8faaa98f4f7107d
rennana567 <2120274879@qq.com>
2025-07-24 16:59:57 +0800

    feat(react): 新增 react 旅游 app 项目
    - 添加项目基本结构和配置文件
    - 集成 eslint、vite 和 react
    - 添加依赖库和 devDependencies
    - 创建 .gitignore 文件

---
+++ react/trip/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{width:'2.66667rem',height:'3.46667rem',backgroundColor:'black'}}></div>
+      <div className='box'></div>
+    </>
+  )
+}
+
+export default App
